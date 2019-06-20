a8a4cd3c60e8c37aa2658789c47f2473daef00b6
syznb <565421243@qq.com>
2019-06-20 16:44:22 +0800

    css and js

--- jewelry/static/js/mob/series.js
+++ jewelry/static/js/mob/series.js
@@ -18,7 +18,7 @@ $(function () {
 
 		$('#main').append('<div id="footer">\n' +
         '             <div class="contact">\n' +
-        '                <a class="ins con" href="https://www.instagram.com/yilanliu_lyl/" target="_blank"><i class="fa fa-instagram fa-lg"></i></a>\n' +
+        '                <a class="ins con" href="https://www.instagram.com/yilanjewellery/" target="_blank"><i class="fa fa-instagram fa-lg"></i></a>\n' +
         '                <a class="wechat con" href="javascript:void(0);"><i class="fa fa-wechat fa-lg"></i></a>\n' +
         '                <a class="weibo con" href="https://weibo.com/u/5627347173" target="_blank"><i class="fa fa-weibo fa-lg"></i></a>\n' +
         '                <a class="email con" href="javascript:void(0);"><i class="fa fa-envelope-o fa-lg"></i></a>\n' +
